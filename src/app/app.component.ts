2ab1adda5ec6ed43bfc204ee031765d58aed21e9
champy1 <nganmoue2@yahoo.fr>
2023-04-26 20:01:32 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'Paie personnel';
+  title = 'Gestion personnel';
 
   constructor(
     private router: Router,
